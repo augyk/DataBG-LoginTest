2a56fe22dbf03ce66f5741018506fc71979e43cf
augyk <58074025+augyk@users.noreply.github.com>
2019-12-07 14:57:24 +0200

    Project Done

---
+++ DataBG-Test/BMLoginTest/BMlogintest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace BMLoginTest
+{
+    [TestClass]
+    public class BMlogintest
+    {
+        private IWebDriver driver;
+ 
+        [TestInitialize]
+        public void SetUp()
+        {
+            driver = new ChromeDriver();
+            
+        }
+        [TestCleanup]
+        public void TearDown()
+        {
+            driver.Quit();
+        }
+        [TestMethod]
+        public void dataBGlogintest()
+        {
+            driver.Navigate().GoToUrl("http://www.data.bg/login");
+            driver.Manage().Window.Size = new System.Drawing.Size(1580, 840);
+            driver.FindElement(By.Id("databg_user")).Click();
+            driver.FindElement(By.Id("databg_user")).SendKeys("wwww");
+            driver.FindElement(By.Id("databg_pass")).Click();
+            driver.FindElement(By.Id("databg_pass")).SendKeys("ssss");
+            driver.FindElement(By.Id("loginbtn")).Click();
+            Assert.AreEqual(driver.FindElement(By.CssSelector("#errormsg > div")).Text,"Паролата и потребителят не съвпадат");
+            driver.FindElement(By.Id("databg_user")).Click();
+            driver.FindElement(By.Id("databg_user")).SendKeys("dsfs#fsdfsd#");
+            driver.FindElement(By.Id("databg_pass")).SendKeys("FDSFS2");
+            driver.FindElement(By.Id("loginbtn")).Click();
+            Assert.AreEqual(driver.FindElement(By.CssSelector("#errormsg > div")).Text, "Паролата и потребителят не съвпадат");
+        }
+           
+    }
+}
---
+++ DataBG-Test/BMLoginTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BMLoginTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BMLoginTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("704e248f-83b0-4114-bcbc-0f8f51e50ec5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
